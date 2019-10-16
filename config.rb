fa46dd331e70052a8f5181bd9edd2c94b8aa7c1b
Mark Green <mark.green@digital.cabinet-office.gov.uk>
2019-10-16 16:57:55 +0100

    Update configs to add contribs block

--- config.rb
+++ config.rb
@@ -1,5 +1,11 @@
 require 'govuk_tech_docs'
 
+GovukTechDocs::SourceUrls.class_eval do
+  def report_issue_url
+    "mailto:govuk-pay-support@digital.cabinet-office.gov.uk?subject=Problem with GOV.UK Pay technical documentation"
+  end
+end
+
 GovukTechDocs.configure(self)
 
 redirect "payment_flow_overview/index.html", to: "payment_flow/index.html"
