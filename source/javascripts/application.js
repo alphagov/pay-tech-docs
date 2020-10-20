90796bc016115ec15fbf7a261eb6942e8e3bf427
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-10-20 13:06:24 +0100

    PP-6284: hook show banner call into application javascripts
    * add govuk-frontend and pay-js-commons as dependencies (these should be
    optional)
    * added the show banner if no cookie consent provided line to
    application javascripts
    
    With @sandorarpa @iqbalgds

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require govuk_tech_docs
-//= require_directory ../../node_modules/pay-js-commons/lib/analytics/dist/
+//= require_directory ../../node_modules/@govuk-pay/pay-js-commons/lib/analytics/dist/
+
+if (window.GovUkPay) {
+  window.GovUkPay.cookies.showBannerIfConsentNotSet()
+}
